833a45d74ae3fa7ee9df496e04b58c8b264c2662
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-16 17:20:24 +1000

    setting up graphql with typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        users: [User]
+    }
+`;
+
+module.exports = typeDefs;
